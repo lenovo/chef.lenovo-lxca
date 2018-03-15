b300a58b1c7b84dd9c62d9b287242a5bfe8e20d8
Prashant Bhosale <prabhosa@users.noreply.github.com>
2018-03-15 14:52:52 +0530

    update to copyright

--- recipes/lxca_cabinet.rb
+++ recipes/lxca_cabinet.rb
@@ -1,10 +1,13 @@
-# <LENOVO COPYRIGHT TO BE INSERTED>
-# Cookbook Name:: lxca
-# Recipe:: lxca_cabinet
+#------------------------------------------------------------
+# Lenovo Copyright
 #
-# Copyright 2017, LENOVO
+# (c) Copyright Lenovo 2017.
 #
-# All rights reserved - Do Not Redistribute
+# LIMITED AND RESTRICTED RIGHTS NOTICE:
+# If data or software is delivered pursuant a General Services
+# Administration (GSA) contract, use, reproduction, or disclosure
+# is subject to restrictions set forth in Contract No. GS-35F-05925.
+#-------------------------------------------------------------
 #
 
 lxca_cabinet 'list_all' do
