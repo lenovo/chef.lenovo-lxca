7cfb427adb2c290798344edfcdafd72ef3fc8be9
umrikar <raghav40_it@yahoo.co.in>
2019-02-28 13:44:03 +0530

    Update metadata.rb
    added support for media mount

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'mrao2@lenovo.com'
 license          'All rights reserved <check with Legal>'
 description      'Installs/Configures lxca'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 source_url 'https://github.com/lenovo/chef.lenovo-lxca'
 issues_url 'https://github.com/lenovo/chef.lenovo-lxca/issues'
